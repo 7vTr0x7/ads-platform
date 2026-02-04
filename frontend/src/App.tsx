8575948c67c04df47e6cda329c6d145cfc4dac59
7vTr0x7 <7vtrox7@gmail.com>
2026-02-04 15:07:59 +0530

    provider + routes setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,35 @@
-import React from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { useAuth } from "./hooks/useAuth";
+import type { JSX } from "react";
 
-const App: React.FC = () => {
-  return <div>App</div>;
-};
+import { ApolloProvider } from "@apollo/client/react";
+import { AuthProvider } from "./provider/auth";
+import { client } from "./apollo/client";
 
-export default App;
+function PrivateRoute({ children }: { children: JSX.Element }) {
+  const { user } = useAuth();
+  return user ? children : <Navigate to="/login" />;
+}
+
+export default function App() {
+  return (
+    <ApolloProvider client={client}>
+      <AuthProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/login" element={<></>} />
+            <Route path="/register" element={<></>} />
+            <Route
+              path="/"
+              element={
+                <PrivateRoute>
+                  <></>
+                </PrivateRoute>
+              }
+            />
+          </Routes>
+        </BrowserRouter>
+      </AuthProvider>
+    </ApolloProvider>
+  );
+}
