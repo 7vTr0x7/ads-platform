7121283df137f53c2d0dc0e54a5b6b7d25582728
7vTr0x7 <7vtrox7@gmail.com>
2026-02-04 10:52:16 +0530

    category resolver

---
+++ backend/src/graphql/modules/category/category.resolvers.ts
@@ -0,0 +1,10 @@
+import { Category } from "../../../models/Category.js";
+
+export const categoryResolvers = {
+  Query: {
+    categories: async () => Category.find(),
+  },
+  Mutation: {
+    createCategory: async (_: any, { input }: any) => Category.create(input),
+  },
+};
